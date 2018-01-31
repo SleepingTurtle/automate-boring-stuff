c5a31867bbd145a392dfb4c2e637a0b6ca1cbf94
Turtle <Sleepingturtle@joshcharlestechie@gmail.com>
2018-01-30 23:33:42 -0500

    early chapters

---
+++ chapter3/sameName3.py
@@ -0,0 +1,16 @@
+def spam():
+    global eggs
+    eggs = 'spam'  # global
+
+
+def bacon():
+    eggs = 'bacon'  # local
+
+
+def ham():
+    print(eggs)  # global
+
+
+eggs = 42 # Global
+spam()
+print(eggs)
