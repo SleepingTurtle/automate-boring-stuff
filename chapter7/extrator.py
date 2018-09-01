9278faa6bd5ba721d8c28037bf0076b1e1df4713
SleepingTurtle <joshcharlestechie@gmail.com>
2018-09-01 18:39:48 -0400

    Finished phone and email extractor

---
+++ chapter7/extrator.py
@@ -0,0 +1,42 @@
+#! python 3
+
+# phoneAndEmailExtractor - Finds phone numbers and email addresses on the clipboard
+
+import re, pyperclip
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(d{3}\))?               # area code
+    (\S|-|\.)?                      # separator
+    (\d{3})                         # first 3 digits
+    (\S|-|\.)                       # separator
+    (\d{4})                         # first 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
+)''', re.VERBOSE)
+
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+               # username
+    @                               # email symbol
+    [a-zA-Z0-9.-]+                  # domain
+    (\.[a-zA-Z]{2,4})
+)''', re.VERBOSE)
+
+# Finds all matches
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# Copy results to the clipboard
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard: ')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
